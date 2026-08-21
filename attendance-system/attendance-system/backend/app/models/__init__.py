a0ca4aaa4c551788fd7e14059c869cec69a9a734
JaDen550 <josephmorsay0@gmail.com>
2026-08-21 03:25:07 +0000

    Update __init__.py

--- attendance-system/attendance-system/backend/app/models/__init__.py
+++ attendance-system/attendance-system/backend/app/models/__init__.py
@@ -1,1 +1,1 @@
-from app.models.models import User, Student, Device, AttendanceRecord, Course, AttendanceSession
+from app.models.models import User, Student, Device, AttendanceRecord, Course, AttendanceSession, CourseEnrollment
